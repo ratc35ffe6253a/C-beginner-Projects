083c9a2593cba6873c3c41b0ca3fabfacad0be2f
specialization24 <wangalibosamuel@yahoo.com>
2025-08-27 09:56:20 +0300

    Project 2: Sum of Numbers from 1 to N

---
+++ 0x00-C-conditionals/sum.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 1, sum = 0;
+	int n;
+	printf("Please enter any number:\n");
+	scanf("%d", &n);
+
+	if (n < 1)
+	{
+		printf("Please enter a positive integer.\n");
+		return 1;
+	}
+
+	while (num <= n)
+	{
+		sum += num;
+		num++;
+	}
+
+	printf("Sum of numbers from 1 to %d is %d\n", n, sum);
+
+	return (0);
+}
