85a21355ef639ef5029a31cb2397f91fd766d3d0
specialization24 <wangalibosamuel@yahoo.com>
2025-08-29 16:14:48 +0300

    🔹 Project 4: Multiplication Table Generator

---
+++ 0x00-C-conditionals/multiplication-table.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num = 1;
+	int n, result;
+
+	printf("Please enter a Positive integer:\n");
+	scanf("%d", &n);
+
+	if (n <= 0)
+	{
+		printf("Please enter a Positive integer\n");
+		return 0;
+	}
+
+	printf("Multiplication table of %d:\n", n);
+
+	while (num <= 10)
+	{
+		result = n * num;
+		printf("%d x %d = %d\n", n, num, result);
+		num++;
+	}
+
+	return (0);
+}
