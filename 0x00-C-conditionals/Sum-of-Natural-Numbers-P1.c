9a5cf5590be3443b6e21ed6381a724e0101d080b
specialization24 <wangalibosamuel@yahoo.com>
2025-08-31 13:20:47 +0300

    🔹 Practice Test 1 (Variation) for Project 5

---
+++ 0x00-C-conditionals/Sum-of-Natural-Numbers-P1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num = 1;
+	int sum = 0;
+	int n;
+
+	printf("Please enter a positive integer:\n");
+	scanf("%d", &n);
+
+	if (n <= 0)
+	{
+		printf("Please enter a positive integer\n");
+		return 0;
+	}
+
+	while (num <= n)
+	{
+		if (num % 2 == 0)
+		{
+			sum += num;
+		}
+		num++;
+	}
+	printf("Sum of even natural numbers upto %d = %d\n", n, sum);
+
+	return (0);
+}
