0c5638cb56a866918ef0d560c9e6fc559ccbc99e
specialization24 <wangalibosamuel@yahoo.com>
2025-09-10 12:59:03 +0300

    Practice Test 2 (Variation) for Project 8

---
+++ 0x00-C-conditionals/LargestAndSmallest-digit.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n, last_digit;
+	int biggest_digit = 0;
+	int smallest_digit = 9;
+	int original_n;
+
+	printf("Please enter a positive number:\n");
+	scanf("%d", &n);
+
+	original_n = n;
+
+	while (n > 0)
+	{
+		last_digit = n % 10;
+
+		if (last_digit > biggest_digit)
+		{
+			biggest_digit = last_digit;
+		}
+
+		if (last_digit < smallest_digit)
+		{
+			smallest_digit = last_digit;
+		}
+
+		n /= 10;
+	}
+	printf("The largest digit in %d is %d\n", original_n, biggest_digit);
+	printf("The smallest digit in %d is %d\n", original_n, smallest_digit);
+
+	return (0);
+}
